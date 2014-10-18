4188cb611593afccccd1aafa6b5a3902e846b049
insanehead <insanehead@gmail.com>
2014-10-18 22:37:35 +0530

    First commit v0.1

---
+++ RailPnr.py
@@ -0,0 +1,20 @@
+import sys
+if (len(sys.argv) != 2) or (type(int(sys.argv[1])) != int):
+	sys.exit("Please provide with a valid Railway PNR as a parameter")
+	
+else:
+	import requests
+	from BeautifulSoup import BeautifulSoup
+
+	with requests.Session() as c:
+		url = 'http://ipnrstatus.in/pnr-result.php' 
+		c.get(url) 
+		req_data = dict(lccp_pnrno1=sys.argv[1], submitpnr="Get PNR Status")
+		page = c.post(url, data=req_data, headers={"Origin":"http://ipnrstatus.in", "Referer":"http://ipnrstatus.in/"}) 
+
+		soup = BeautifulSoup(page.text)
+		print soup.title.text
+
+		tab = soup.findAll('table')
+
+		print tab[1].fetch('b')[2].text
